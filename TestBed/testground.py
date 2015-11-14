e03a2d5129147c1ed5a9ce5ca4238d82098d0214
Nathan Kau <nathankau@gmail.com>
2015-11-14 12:27:25 -0800

    delete a bunch of stuff

--- TestBed/testground.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from glob import glob
-K= np.array([[1.14377690e+03,   0.00000000e+00,   9.71017458e+02],
- [  0.00000000e+00,   1.14128154e+03,   5.18938715e+02],
- [  0.00000000e+00,  0.00000000e+00,   1.00000000e+00]])
-
-
-names = glob('calibration_samples/*')
-print names
